7a86bdef70c5e814c8ff8fbbf7c31c5bd0daa624
Maurício Pires Formes <mauriciopiresformes@hotmail.com>
2022-06-23 22:55:54 -0300

    subindo arquivos pela primeira vez

---
+++ 01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FSPHP: Iniciando um projeto!</title>
+    <link rel="stylesheet" href="assets/style.css" />
+</head>
+
+<body>
+
+    <?php
+
+    $start = "Vamos começar";
+
+    echo "<h1> Olá mundo! {$start}</h1>";
+    echo "<p id='js'>Loading...</p>";
+
+
+    ?>
+    <script src="assets/scripts.js"></script>
+</body>
+</html>
---
+++ 02-iniciando-do-zero-com-php/02-02-definindo-ambiente/index.php
@@ -0,0 +1,7 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName(className: "02.02 - Definindo Ambiente");
+
+fullStackPHPClassSession(session: "Debug Section", line: __LINE__);
+
+var_dump($_SERVER);
